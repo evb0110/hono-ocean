1b46e8ba0227a3487126fede3b65d0e68a3be48c
Eugene Barsky <7149553@gmail.com>
2024-11-04 12:16:54 +0300

    auto

--- src/index.ts
+++ src/index.ts
@@ -1,13 +1,21 @@
 import { Hono } from 'hono'
 
-const app = new Hono()
+const app = new Hono();
+
+const data: any[] = ['first record'];
 
 app.get('/', (c) => {
   return c.text('Hello Hono 1!')
 });
 
-app.post('/add-stats', (c) => {
+app.post('/add-stats', async (c) => {
+    const body = await c.req.json();
+    data.push(body);
     return c.text('Stats added!')
+});
+
+app.get('/get-stats', (c) => {
+    return c.json(data);
 });
 
 export default app
