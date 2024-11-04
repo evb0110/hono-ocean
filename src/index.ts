24cde6aeec32e13e6c2a6714a3cf640788e6fec5
Eugene Barsky <7149553@gmail.com>
2024-11-04 11:48:00 +0300

    auto

--- src/index.ts
+++ src/index.ts
@@ -4,6 +4,10 @@ const app = new Hono()
 
 app.get('/', (c) => {
   return c.text('Hello Hono 1!')
-})
+});
+
+app.post('/add-stats', (c) => {
+    return c.text('Stats added!')
+});
 
 export default app
